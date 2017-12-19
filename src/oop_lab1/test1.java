53030770d2b463ae904d7bc784b0e45bfdd40834
natanong <natanongchaitham@gmail.com>
2017-12-19 23:54:29 +0700

    Test Input

--- src/oop_lab1/test1.java
+++ src/oop_lab1/test1.java
@@ -2,6 +2,8 @@ package oop_lab1;
 
 public class test1 {
     public static void main(String[] args) {
+        System.out.println("Hello Wordl, I'm AI");
+        System.out.println("Information System");
 
     }//main
 }//class
