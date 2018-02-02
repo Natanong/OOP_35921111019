546c4cfaec0b8c52b1632b90b678e993693620f6
natanong <natanongchaitham@gml.com>
2018-02-02 10:32:42 +0700

    Abstract Class:pet

---
+++ src/oop_lab8/PetApp.java
@@ -0,0 +1,11 @@
+package oop_lab8;
+
+public class PetApp {
+    public static void main(String[] args) {
+
+        Dog dog = new Dog("kaya",2);
+        System.out.println(dog.getName());
+        System.out.println(dog.getAge());
+        dog.makeNoise();
+    }
+}//class
